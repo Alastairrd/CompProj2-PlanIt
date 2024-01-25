f7503a50ad99ad5f5de4d9114d5c50e400e23a2d
Alastairrd <alastairros-davison@hotmail.com>
2024-01-25 18:26:50 +0000

    add data val attribute, started working on saving values from calendar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,38 @@
-
 //Function fires when page is loaded
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
+	//get all dateTime elements from index.html
+	dateTimes = document.getElementsByClassName("dateTime");
 
-    //get all dateTime elements from index.html
-    dateTimes = document.getElementsByClassName("dateTime");
-
-    //add mousedown and mouseover event listeners to every dateTime element, which fires the setActive function
-    for(i=0; i < dateTimes.length; i++){
-        dateTimes[i].addEventListener('mouseover', setActive, false);
-        dateTimes[i].addEventListener('mousedown', setActive, false);
-    }
+	//add mousedown and mouseover event listeners to every dateTime element, which fires the setActive function
+	for (i = 0; i < dateTimes.length; i++) {
+		dateTimes[i].addEventListener("mouseover", setActive, false);
+		dateTimes[i].addEventListener("mousedown", setActive, false);
+		dateTimes[i].setAttribute("data-val", 0);
+	}
 });
 
 //if dateTime element detects mousedown or mouseover
 function setActive(e) {
-    //it checks if the left mouse button is currently down
-    if(e.buttons == 1){
+	//it checks if the left mouse button is currently down
+	if (e.buttons == 1) {
+		//if element is already active, remove active (css class)
+		if (this.classList.contains("active")) {
+			this.classList.remove("active");
 
-        //if element is already active, remove active (css class)
-        if(this.classList.contains("active")){
-            this.classList.remove("active")
+			//set data-val attribute
+			this.setAttribute("data-val", 0);
 
-            //otherwise, make active (css class)
-        } else {
-            this.classList.add("active");
-        }
-    }
+			//otherwise, make active (css class)
+		} else {
+			this.classList.add("active");
+			this.setAttribute("data-val", 1);
+		}
+	}
 }
 
+function saveTest(e){
+
+    dateTimes = document.getElementsByClassName("dateTime");
+
+    
+}
