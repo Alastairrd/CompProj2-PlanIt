ab127d1b16681cc4b4e9d23013290cb3709cd526
Alastairrd <alastairros-davison@hotmail.com>
2024-01-26 12:31:42 +0000

    comments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+	//get all date time objects
     dateTimes = document.getElementsByClassName("dateTime");
 
+	//set event listeners for clicks and mousedown
     for(i=0; i < dateTimes.length; i++)
     {
         dateTimes[i].addEventListener('mouseover', setActive, false);
         dateTimes[i].addEventListener('mousedown', setActive, false);
     }
 
+
+	//archie stuff
     populateDate = document.getElementsByClassName("dateTime2");
 
     for(i = 0; i < populateDate.length; i++)
@@ -18,11 +22,18 @@ document.addEventListener("DOMContentLoaded", function(){
 
 });
 
-
+//
 function setActive(e) {
+	//if left mouse button is being pressed
     if(e.buttons == 1){
+
+		//if grid-item is active already
         if(this.classList.contains("active")){
+
+			//remove it
             this.classList.remove("active")
+
+			//otherwise set active
         } else {
             this.classList.add("active");
         }
