01a0393cf628247fac1187c8e347919efd43c6c1
Alastairrd <alastairros-davison@hotmail.com>
2024-02-01 18:14:42 +0000

    Massiven restructure - > used working node web server app as basic. Initial route added, csv reading functionality added into route Csv reading functionality added

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-//import the modules that we need
+// Import the modules we need
 const express = require("express");
-const app = express();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
-const path = require('path');
 const fs = require('fs');
-const Papa = require('papaparse');
+const path = require('path');
+const { log } = require("console");
+
+// Create the express application object
+const app = express();
+const port = 8000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const filePath = path.join(__dirname, 'test.csv');
 
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) 
-    {
-        console.error('Error reading the file:', err);
-        return;
-    }
-    
-    const lines = data.split('\n').filter(line => line.trim()); // Split and filter out empty lines
-    const processedData = lines.slice(1).map(line => {
-        const entries = line.split(',').map(entry => entry.trim()); // Split and trim each entry
-        return entries.slice(1); // Ignore the first entry (time slot)
-    });
-    console.log(processedData);
-});
+
+// Set up css and scripts path
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/scripts"));
+
+// Set the directory where Express will pick up HTML files
+// __dirname will get the current directory
+app.set("views", __dirname + "/views");
+
+// Tell Express that we want to use EJS as the templating engine
+app.set("view engine", "ejs");
+
+// Tells Express how we should process html files
+// We want to use EJS's rendering engine
+app.engine("html", ejs.renderFile);
+
+const csvData = null;
+
+
+// Requires the main.js file inside the routes folder passing in the Express app and data as arguments.  All the routes will go in this file
+require("./routes/main")(app, csvData, filePath, fs);
+
+// Start the web app listening
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ routes/main.js
@@ -0,0 +1,47 @@
+module.exports = function (app, csvData, filePath, fs) {
+    // Handle our routes
+
+    app.get("/", async function (req, res) {
+
+        const testData = await new Promise((resolve, reject) => {
+
+            fs.readFile(filePath, 'utf8', (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+
+
+					//splits the data based on each new line, then iterate through the array of lines, and trim each one
+					const lines = data.split('\n').filter(l => l.trim());
+
+					//slice off the first line
+					linesSliced = lines.slice(1);
+
+					//uses map to take each line, split into entries using comma delimiter
+					handledData = linesSliced.map(l => {
+						const entries = l.split(',');
+
+						//then we trim every entry incase
+						const trimmedEntries = entries.map(e => e.trim());
+
+						//then we slice off the initial column of time rows
+						finalEntries = trimmedEntries.slice(1);
+
+						//return the trimmed entries without initial column
+						return finalEntries;
+					})
+
+					//send the now processed data to the promise
+                    resolve(handledData);
+                }
+            });
+
+        });
+
+
+		//TODO Archie/Al can now use this testData to populate!
+		//need to restructure the html/css to use classes
+        console.log(testData);
+    });
+}
+
